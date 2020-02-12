7a8a37c0248abb226ec8518b24613d5236cc4995
YaKsenia <xenia.iakovleva@gmail.com>
2020-02-12 14:55:11 +0000

    added a new model and view for a future murich page

--- blog/views.py
+++ blog/views.py
@@ -18,13 +18,21 @@ class HomePageView(ListView):
     template_name = 'home.html'
     ordering = ['-published_date']
 
+class MurichPageView(ListView):
+    #Post.objects.all().order_by('published_date')
+    model = Murich
+    template_name = 'murich.html'
+    ordering = ['-published_date']
+
+
+'''
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(user=request.user, published_date__lte=timezone.now()).order_by('published_date')
     Post.objects.all()
     #Post.objects.get(pk=pk)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+'''
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
