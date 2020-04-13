1aaa854bedaccd5ecc57e4e5c7616c21821fa33e
YaKsenia <xenia.iakovleva@gmail.com>
2020-04-13 14:09:27 +0000

    added forms for the new model Huckyou and for images

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+'''
 from django import forms
 
 from .models import Post
@@ -7,3 +8,22 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+'''
+
+from django import forms
+from .models import HuckYou, Images
+
+class PostForm(forms.ModelForm):
+    title = forms.CharField(max_length=128)
+    body = forms.CharField(max_length=20000, label="Article text")
+
+    class Meta:
+        model = HuckYou
+        fields = ('title', 'text', )
+
+
+class ImageForm(forms.ModelForm):
+    image = forms.ImageField(label='Image')
+    class Meta:
+        model = Images
+        fields = ('image', )
