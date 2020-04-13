ae887017fa2cc5abbdc3809d8014fb1ae8e3b23d
YaKsenia <xenia.iakovleva@gmail.com>
2020-04-13 14:11:20 +0000

    added 2 new models - for huckyou posts and images

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,33 @@ class Murich(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class HuckYou(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+
+def get_image_filename(instance, filename):
+    title = instance.post.title
+    slug = slugify(title)
+    return "post_images/%s-%s" % (slug, filename)
+
+
+class Images(models.Model):
+    post = models.ForeignKey(HuckYou, default=None)
+    image = models.ImageField(upload_to=get_image_filename,
+                              verbose_name='Image')
