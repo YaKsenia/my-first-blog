1d10bf99cd8a88cd68b1895a05fc9e891c76a697
YaKsenia <xenia.iakovleva@gmail.com>
2020-04-18 15:35:14 +0000

    added the ckeditor into admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Post
 from .models import Murich
+from .models import HuckYou
+from .models import Images
 
+from django.contrib import admin
+from blog.models import HuckYou
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    change_form_template = 'blog/admin/change_form.html'
+
 admin.site.register(Murich, AuthorAdmin)
-#admin.site.register(Murich)
 admin.site.register(Post)
+admin.site.register(HuckYou)
+admin.site.register(Images)
