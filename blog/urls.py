bde5a192065b91d07e43653284b6c1d1c7494846
YaKsenia <xenia.iakovleva@gmail.com>
2020-04-18 15:39:25 +0000

    added the ckeditor into urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,20 +1,31 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include, url
+
+
 
 
 # posts/urls.py
 
-from .views import HomePageView
+#from .views import HomePageView
 from .views import MurichPageView
+from .views import AraPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', views.post_list, name='post_list'),
+    #path('', HomePageView.as_view(), name='home'),
     path('murich', MurichPageView.as_view(), name='murich'),
-    path('admin/blog/murich', MurichPageView.as_view(), name='admin/blog/murich'),
+    path('aryan', AraPageView.as_view(), name='aryan'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
 
 
 '''
+path('admin/blog/murich', MurichPageView.as_view(), name='admin/blog/murich')
 urlpatterns = [
     path('', views.post_list),
     path('post/', views.post_detail),
