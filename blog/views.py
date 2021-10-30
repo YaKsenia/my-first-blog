f7fdcc87990939c2a5617f813b0258a503b578cf
YaKsenia <xenia.iakovleva@gmail.com>
2021-10-30 18:02:13 +0000

    updated views with hitcount code to get views number

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
 from django.utils import timezone
 from .models import HuckYou
 from .models import Murich
@@ -6,15 +6,15 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from .forms import ImageForm
 from django.shortcuts import redirect
-from django.forms.models import modelformset_factory
 from django.views.generic import ListView
 from .models import Post
 from .models import Images
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-#from tabination.views import TabView
+from hitcount.views import HitCountDetailView
+
 
 '''
+
+
 class HomePageView(ListView):
     #Post.objects.all().order_by('published_date')
     model = Post
@@ -35,8 +35,8 @@ class AraPageView(ListView):
 
 
 def post_list(request):
-    posts = HuckYou.objects.order_by('published_date')
-    images = Images.objects.order_by('image')
+    posts = HuckYou.objects.order_by('-published_date')
+    images = Images.objects.order_by('-image')
     #.filter(author=request.user, published_date__lte=timezone.now())
    # HuckYou.objects.all()
     #Post.objects.get(pk=pk)
@@ -50,12 +50,12 @@ def post_images(request, slug):
 
 
 def practice(request):
-    posts = HuckYou.objects.order_by('published_date')
+    practice_posts = HuckYou.objects.filter(label='practice').order_by('published_date')
     images = Images.objects.order_by('image')
     #.filter(author=request.user, published_date__lte=timezone.now())
    # HuckYou.objects.all()
     #Post.objects.get(pk=pk)
-    return render(request, 'blog/practice.html', {'posts': posts, 'images' : images})
+    return render(request, 'blog/practice.html', {'practice_posts': practice_posts, 'images' : images})
 
 
 def theory(request):
@@ -73,11 +73,28 @@ def about(request):
     #Post.objects.get(pk=pk)
     return render(request, 'blog/about.html', {'posts': posts})
 
-
+'''
 def post_detail(request, pk):
     post = get_object_or_404(HuckYou, pk=pk)
     posts = HuckYou.objects.order_by('published_date')
     return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts})
+'''
+
+
+class PostDetailView(HitCountDetailView):
+    model = HuckYou
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    # set to True to count the hit
+    count_hit = True
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context.update({
+        'popular_posts': HuckYou.objects.order_by('-hit_count_generic__hits')[:3],
+        })
+        return context
+
 
 
 def post_new(request):
